84451058d869c24e53a068c38ac8c98572f93310
Kwinten Olaerts <kwintenolaerts@hotmail.com>
2019-02-11 09:43:06 +0100

    fully function pokedex

--- assets/async.js
+++ assets/async.js
@@ -1,14 +1,82 @@
-$.get("https://pokeapi.co/api/v2/pokemon/ditto/",function(data){
-    $("#pokedex").text(data.name+" "+data.weight)
-    $("#photo").attr("src", data.sprites.front_default)
-    $("#moves").append(data.moves)
+// $.get("https://pokeapi.co/api/v2/pokemon/ditto/",function(data){
+//     $("#pokedex").text(data.name+" "+data.weight)
+//     $("#photo").attr("src", data.sprites.front_default)
+//     $("#moves").append(data.moves)
 
-    console.log(data)
-    console.log(data.sprites.front_default)
-});
-
-
-
-console.log("test");
+//     console.log(data)
+//     console.log(data.sprites.front_default)
+// });
 
 
+// Comment: own javascript, before I imported a pokedex
+// console.log("test");
+
+
+$(document).ready(function(){
+    function capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    };
+    var trueInput= $(".trueInput");
+    trueInput.keyup(function(e){    
+          if(e.keyCode == 13){ 
+             research();
+             trueInput.val("");
+          };
+    });
+    function research(){
+      var input= trueInput.val();
+      var description= $('.descriptionText');
+      var image=$(".pokeImage");
+      var types=$('.typeText');
+      input=input.toLowerCase();
+      var api= "https://pokeapi.co/api/v2/pokemon/"+input+"/";
+      
+      $.ajax({
+        type:"GET",
+        url:api,
+        async:false,
+        dataType:"json",
+        success:function(data){
+                  var name= data.forms[0].name;
+                  name=capitalizeFirstLetter(name);
+                  var api2="https://pokeapi.co/api/v2/characteristic/"+data.id
+                  $.getJSON(api2, function(data2){ 
+                    description.html(data2.descriptions[1].description);
+                  });
+                  image.attr("src", data.sprites.front_default);
+                  var shiny= 0;
+                  function backview(){
+                    if(shiny==0){image.attr("src", data.sprites.back_default)}
+                    else{image.attr("src", data.sprites.back_shiny)};
+                  };
+                  function frontview(){
+                    if(shiny==0){image.attr("src",data.sprites.front_default)}
+                    else{image.attr("src", data.sprites.front_shiny)};
+                  };
+                  $('.select').click(backview);
+                  $('.start').click(frontview);
+                  $('.name').html("#"+data.id+" "+name);
+                  $('.heightText').html(data.height/10+" m");
+                  $('.weightText').html(data.weight/10+" kg");
+                  var type1=data.types[1].type.name;
+                  var type2=data.types[0].type.name;
+                  type1=capitalizeFirstLetter(type1);
+                  type2=capitalizeFirstLetter(type2);
+                  types.html(type1).append(" "+type2);
+                  $('.blueButton').click(shinyness);
+                  function shinyness(){
+                    if(shiny==0){shiny=1}else{shiny=0};
+                    frontview();
+                  };
+                },
+        error: function(errorMessage){
+                setTimeout(function(){ 
+                 $('.screen').toggle();
+                }, 10);
+                setTimeout(function(){
+                 $('.screen').toggle();
+                }, 2000);
+               }
+        });
+    };
+  });
